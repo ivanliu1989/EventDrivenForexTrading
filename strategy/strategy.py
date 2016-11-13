93febdba904025cb57cc0cdbf7eb7287c4e6bc7d
Ivan Liu <ivan.liuyanfeng@gmail.com>
2016-11-13 16:08:29 +1100

    update for backtesting

---
+++ strategy/strategy.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from event.event import SignalEvent
+
+
+class TestStrategy(object):
+    def __init__(self, instrument, events):
+        self.instrument = instrument
+        self.events = events
+        self.ticks = 0
+        self.invested = False
+
+    def calculate_signals(self, event):
+        if event.type == 'TICK':
+            self.ticks += 1
+            if self.ticks % 5 == 0:
+                if self.invested == False:
+                    signal = SignalEvent(self.instrument, "market", "buy")
+                    self.events.put(signal)
+                    self.invested = True
+                else:
+                    signal = SignalEvent(self.instrument, "market", "sell")
+                    self.events.put(signal)
+                    self.invested = False
